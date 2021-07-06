e08ee2a2952f0e200f28ecf2688f8b3333822a45
Isreal6ix <isrealsix@gmail.com>
2021-07-06 16:21:13 +0700

    section 1 completed

---
+++ index.js
@@ -0,0 +1,31 @@
+// Navigation dropdown hover
+
+const allDropdown = document.querySelectorAll('.dropdown-hover');
+const section1 = document.querySelector('.section-1');
+
+// section1.addEventListener('mouseover', ev => {
+// 	const drop = ev.target.closest('.dropdown-hover');
+// 	if (!drop) return;
+// 	drop.lastElementChild.style.cssText = `opacity: 1; visibility: visible;`;
+// });
+
+// section1.addEventListener('mouseout', ev => {
+// 	const out = ev.target.closest('.dropdown-hover');
+// 	if (!out) return;
+// 	out.lastElementChild.style.cssText = `opacity: 0; visibility: hidden`;
+// });
+
+// Refactored
+
+const displayDropdown = eventType => {
+	section1.addEventListener(eventType, ev => {
+		const target = ev.target.closest('.dropdown-hover');
+		if (!target) return;
+		let opacity = eventType == 'mouseover' ? 1 : 0;
+		let visibility = eventType == 'mouseover' ? 'visible' : 'hidden';
+		target.lastElementChild.style.cssText = `opacity: ${opacity}; visibility: ${visibility}`;
+	});
+};
+
+displayDropdown('mouseover');
+displayDropdown('mouseout');
