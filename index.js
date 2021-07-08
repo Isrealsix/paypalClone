1d1ba3b8a41b1a6e1e336aa2842257527a9b0a25
Isreal6ix <isrealsix@gmail.com>
2021-07-08 20:57:00 +0700

    added functionality for sidebar to toggle appearance and to remove the heading on wider screen

--- index.js
+++ index.js
@@ -16,26 +16,6 @@ const section1 = document.querySelector('.section-1');
 // 	out.lastElementChild.style.cssText = `opacity: 0; visibility: hidden`;
 // });
 
-// Refactored
-
-const displayDropdown = eventType => {
-	section1.addEventListener(eventType, ev => {
-		const target = ev.target.closest('.dropdown-hover');
-		if (!target) return;
-		let opacity = eventType == 'mouseover' ? 1 : 0;
-		let visibility = eventType == 'mouseover' ? 'visible' : 'hidden';
-		let background =
-			eventType == 'mouseover'
-				? 'linear-gradient(to right, #066399, #2f8fdf, #066399)'
-				: 'none';
-		target.lastElementChild.style.cssText = `opacity: ${opacity}; visibility: ${visibility};`;
-		navbar.style.cssText = `background: ${background}`;
-	});
-};
-
-displayDropdown('mouseover');
-displayDropdown('mouseout');
-
 // pages
 const loginPage = document.querySelector('.login-page');
 const frontPage = document.querySelector('.front-page');
@@ -66,3 +46,56 @@ container.addEventListener('click', ev => {
 		signupPage.style.display = 'none';
 	}
 });
+
+if (window.innerWidth < 1000) {
+	const menu = document.querySelector('.menu');
+	const navbarInner = document.querySelectorAll('.navbar__inner');
+
+	menu.addEventListener('click', () => {
+		navbar.classList.toggle('change');
+
+		if (!navbar.classList.contains('change')) {
+			document.querySelectorAll('.navbar__dropdown').forEach(dropdown => {
+				dropdown.style.left = '-20rem';
+			});
+		}
+
+		document
+			.querySelectorAll('.navbar__inner-heading-link')
+			.forEach(heading => {
+				heading.addEventListener('click', () => {
+					heading.parentElement.parentElement.style.left = '-20rem';
+				});
+			});
+	});
+
+	// Show inner Lists
+	navbarInner.forEach(inner => {
+		inner.addEventListener('click', () => {
+			inner.nextElementSibling.style.left = '0';
+		});
+	});
+} else {
+	// Refactored
+	const displayDropdown = eventType => {
+		section1.addEventListener(eventType, ev => {
+			const target = ev.target.closest('.dropdown-hover');
+			if (!target) return;
+			let opacity = eventType == 'mouseover' ? 1 : 0;
+			let visibility = eventType == 'mouseover' ? 'visible' : 'hidden';
+			let rotate = eventType == 'mouseover' ? 'rotate(180deg)' : 'rotate(0)';
+			// console.log(target.firstElementChild);
+			let background =
+				eventType == 'mouseover'
+					? 'linear-gradient(to right, #066399, #2f8fdf, #066399)'
+					: 'none';
+			target.lastElementChild.style.cssText = `opacity: ${opacity}; visibility: ${visibility};`;
+			navbar.style.cssText = `background: ${background}`;
+			// Rotate Icon
+			target.firstElementChild.firstElementChild.style.cssText = `transform: ${rotate}`;
+		});
+	};
+
+	displayDropdown('mouseover');
+	displayDropdown('mouseout');
+}
